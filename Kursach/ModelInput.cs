a7af7b1eb7971e56045447faa7986e32f9736ba3
SemyonSuvorov <125412363+SemyonSuvorov@users.noreply.github.com>
2024-01-08 23:29:11 +0300

    Add files via upload

---
+++ Kursach/ModelInput.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+
+public class ModelInput
+{
+    public ModelInput(string utterance)
+    {
+        Sentence = utterance;
+    }
+
+    [LoadColumn(2)]
+    [ColumnName(@"Description")]
+    public string Description { get; set; }
+
+    [LoadColumn(1)]
+    [ColumnName(@"Sentence")]
+    public string Sentence { get; set; }
+
+    [LoadColumn(0)]
+    [ColumnName(@"Label")]
+    public float Label { get; set; }
+
+}
---
+++ Kursach/ModelOutput.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Data;
+using System.Numerics;
+
+public class ModelOutput
+{
+    [ColumnName(@"Sentence")]
+    public VBuffer<Single> Sentence { get;  set; }
+
+    [ColumnName(@"Label")]
+    public uint Label { get; set; }
+
+    [ColumnName(@"PredictedLabel")]
+    public float PredictedLabel { get; set; }
+
+    [ColumnName(@"Score")]
+    public float[] Score { get; set; }
+
+}

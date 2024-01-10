b8610a7bdec21344796dfbd4fc1dbe9a2fb73b61
Semyon <semen.suvoroff@yandex.ru>
2024-01-10 22:41:08 +0300

    fixes

--- Kursach/Program.cs
+++ Kursach/Program.cs
@@ -14,6 +14,7 @@ IDataView trainData = mlContext.Data.LoadFromTextFile<ModelInput>(
     separatorChar: '\t',
     hasHeader: false
 );
+
 IDataView testData = mlContext.Data.LoadFromTextFile<ModelInput>(
     "test.tsv",
     separatorChar: '\t',
@@ -28,22 +29,29 @@ var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label", "Label")
                           .Append(mlContext.Transforms.Conversion.MapValueToKey("Feature"))
                           .Append(mlContext.Transforms.Text.ProduceNgrams("Feature"))
                           .Append(mlContext.Transforms.NormalizeLpNorm("Feature"))
-                          .Append(mlContext.MulticlassClassification.Trainers.SdcaNonCalibrated(featureColumnName: "Feature"))
-                          .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+                          .Append(mlContext.MulticlassClassification.Trainers.OneVersusAll(
+                              mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression(featureColumnName: "Feature",
+                              l2Regularization: 0, l1Regularization: 0))
+                          .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel")));
 
 Console.WriteLine("Training model...");
 ITransformer model = pipeline.Fit(trainData);
 Console.WriteLine("Evaluating model performance...");
 
-
 IDataView transformedTest = model.Transform(testData);
 MulticlassClassificationMetrics metrics = mlContext.MulticlassClassification.Evaluate(transformedTest);
 
 // Display Metrics
+Console.WriteLine();
 Console.WriteLine($"Average Macro Accuracy: {metrics.MacroAccuracy}");
 Console.WriteLine($"Log Loss: {metrics.LogLoss}");
-Console.WriteLine();
+Console.WriteLine("Log Loss per class:");
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine($"Class - {Enum.GetValues<ArticleIntents>()[i]}, Log Loss for class - {metrics.PerClassLogLoss[i]}");
+}
 
+Console.WriteLine();
 Console.WriteLine("Classes:");
 foreach (ArticleIntents value in Enum.GetValues<ArticleIntents>())
 {
@@ -66,13 +74,16 @@ do
     Console.WriteLine();
     Console.WriteLine("What do you want to say ? (Type Q to Quit)");
     input = Console.ReadLine()!;
-
+    if(input.ToLowerInvariant() == "q" || string.IsNullOrWhiteSpace(input))
+    {
+        break;
+    }
     ModelInput sampleData = new(input);
     ModelOutput result = engine.Predict(sampleData);
 
     Console.WriteLine($"Matched intent - {(ArticleIntents)result.PredictedLabel}");
     Console.WriteLine();
 }
-while (!string.IsNullOrWhiteSpace(input) && input.ToLowerInvariant() != "q");
+while (true);
 
-Console.WriteLine("Bye");
+Console.WriteLine("Bye!");
